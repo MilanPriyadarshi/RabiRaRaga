94ee3741cdb6206f1109fd4dd1c2a978473deebc
Milan <milansutar7@gmail.com>
2021-06-27 06:55:28 +0100

    deploy

--- RabiRaRaga/settings.py
+++ RabiRaRaga/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0@gyab4zzx301rx*vu2-0-&yf)of@j4&h5r4!%qh&c&o9@rudx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','rabiraraga.herokuapp.com']
 
 
 # Application definition
